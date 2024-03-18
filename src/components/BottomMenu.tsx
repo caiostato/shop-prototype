78abf3fa9a3a52fbb8bdbd1e5010cf9c87e9fb80
caiostato <zombieiron9@gmail.com>
2024-03-18 17:34:20 -0300

    fix: button's color updated

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -12,7 +12,7 @@ const BottomMenu = () => {
     <div className="w-screen h-16 bg-zinc-50 absolute bottom-0 flex flex-row justify-around shadow-sm ">
       <div
         className={`my-2 p-2 rounded-full ${
-          iconSelected == "home" ? "bg-[#582FFF]/20" : ""
+          iconSelected == "home" ? "bg-[#F89595]/30" : ""
         }`}
         onClick={() => {
           handleClickIcon("home");
@@ -20,7 +20,7 @@ const BottomMenu = () => {
       >
         <Home2
           size="32"
-          color={`${iconSelected == "home" ? "#582FFF" : "black"}`}
+          color={`${iconSelected == "home" ? "#F89595" : "black"}`}
           variant={`${iconSelected == "home" ? "Bold" : "Linear"}`}
           className="my-auto"
           onClick={() => {
@@ -31,7 +31,7 @@ const BottomMenu = () => {
 
       <div
         className={`my-2 p-2 rounded-full duration-100 ease-in ${
-          iconSelected == "cart" ? "bg-[#582FFF]/20" : ""
+          iconSelected == "cart" ? "bg-[#F89595]/30" : ""
         }`}
         onClick={() => {
           handleClickIcon("cart");
@@ -39,7 +39,7 @@ const BottomMenu = () => {
       >
         <ShoppingCart
           size="32"
-          color={`${iconSelected == "cart" ? "#582FFF" : "black"}`}
+          color={`${iconSelected == "cart" ? "#F89595" : "black"}`}
           variant={`${iconSelected == "cart" ? "Bold" : "Linear"}`}
           className="my-auto duration-100 ease-in"
           onClick={() => {
@@ -50,7 +50,7 @@ const BottomMenu = () => {
 
       <div
         className={`my-2 p-2 rounded-full ${
-          iconSelected == "user" ? "bg-[#582FFF]/20" : ""
+          iconSelected == "user" ? "bg-[#F89595]/30" : ""
         }`}
         onClick={() => {
           handleClickIcon("user");
@@ -58,7 +58,7 @@ const BottomMenu = () => {
       >
         <User
           size="32"
-          color={`${iconSelected == "user" ? "#582FFF" : "black"}`}
+          color={`${iconSelected == "user" ? "#F89595" : "black"}`}
           variant={`${iconSelected == "user" ? "Bold" : "Linear"}`}
           className="my-auto"
           onClick={() => {
