25e5949a22998ba3465959b4f55e08d73fddfa24
caiostato <zombieiron9@gmail.com>
2024-03-18 17:49:21 -0300

    fix: clean root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,14 @@
-"use client";
 import "./globals.css";
-import BottomMenu from "@/components/BottomMenu";
-import TopMenu from "@/components/TopMenu";
-import Sidebar from "@/components/Sidebar";
-import { useState } from "react";
-import { useAnimate } from "framer-motion";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [scope, animate] = useAnimate();
-  const [status, setStatus] = useState(false);
-  const handleClickOpen = () => {
-    animate("div", { x: "100%" }, { ease: "circInOut" });
-  };
-  const handleClickClose = () => {
-    animate("div", { x: "0%" });
-  };
-
-  const handleClickOpenMenu = () => {
-    handleClickOpen();
-    setStatus(true);
-  };
-
   return (
     <html lang="pt">
-      <body className="bg-zinc-50 container m-0 p-0 font-sans antialiased overflow-x-hidden overflow-y-hidden">
+      <body className="bg-zinc-100 container m-0 p-0 font-sans antialiased overflow-x-hidden overflow-y-hidden">
         {children}
-        {/* <Sidebar
-            status={status}
-            scope={scope}
-            setStatus={() => {
-              setStatus(false);
-            }}
-          />
-          <div className={`${status ? "blur-sm" : "duration-100"}`}>
-            <TopMenu handleClickOpenMenu={handleClickOpenMenu} />
-            {children}
-            <BottomMenu />
-          </div> */}
       </body>
     </html>
   );
