77c1af38a00a8796a93e5f7f814fe2f8c8ff2251
caiostato <zombieiron9@gmail.com>
2024-10-30 09:28:27 -0300

    feat: new logo

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import React from "react";
 
-import Logo from "@/assets/logo-wordmark.jpg";
+import Logo from "@/assets/logo-wordmark.svg";
 import SearchBar from "./SearchBar";
 
 const Menu = () => {
   return (
-    <div className="min-w-full bg-cream px-32 pt-2">
-      <div className=" w-full flex items-center justify-between py-2 border-b-[1px] border-gold-600/10">
-        <Image src={Logo} alt={"logo"} width={200} height={50} />
+    <div className="min-w-full bg-cream  pt-2 border-gold-600/10">
+      <div className=" w-full flex items-center justify-between py-2 border-b-[1px] px-32">
+        <Image src={Logo} alt={"logo"} width={240} height={60} />
         <div>
           <SearchBar />
         </div>
